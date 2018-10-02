81a4f9cff6dd2863aab2f25dd1772c11c9154dd1
kevinvietvu <kevinvietvu@yahoo.com>
2018-10-02 16:55:47 -0700

    added input for money and displays random orders

--- src/index.js
+++ src/index.js
@@ -7,81 +7,176 @@ import axios from 'axios';
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.xsrfCookieName = "csrftoken";
 
+/*
+build complex UIs from small and isolated pieces of code called components (React.Component)
+A component takes in parameters, called props (short for “properties”), and returns a hierarchy
+of views to display via the render method.
+*/
 class FetchDemo extends React.Component {
-  state = {
-    company : "McDonalds",
-    stateName : "CA",
-    menu: {},
+  constructor(props) {
+      super(props)
+      this.state = {
+        company : "McDonalds",
+        stateName : "CA",
+        amount : '',
+        menu: {},
+        menuTypes : {},
+        randomOrders : {},
+      };
+
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
-// Fetch server data in the componentDidMount lifecycle method, method executed only once
-  componentDidMount() {
-    axios.get("http://localhost:8000/api/")
-      .then(res => {
-		/*
-		The component’s state is updated by calling this.setState with the new array of posts.
-		This triggers a re-render, and then the posts are visible.
-		*/
-        const menuData = res.data
-        this.setState({ menu: menuData },
-            ()=>{console.log()}
-        );
+  // Fetch server data in the componentDidMount lifecycle method, method executed only once
+    componentDidMount() {
+      axios.get("http://localhost:8000/api/")
+        .then(res => {
+  		/*
+  		  Set initial state with server data
+  		*/
+          const menuData = res.data
+          this.setState({ menu: menuData },
+              ()=>{console.log()}
+          );
 
-      }).catch(function(error) {
-    console.log(error);
-  });
+          const types = {}
 
+          //using forEach instead of key => {} prevents return warning
+          Object.keys(menuData).forEach(function(menuType) {
+            types[menuType] = false
+          });
 
-  axios({
-    method: 'post',
-    url: 'http://localhost:8000/api/test',
-    data: {
-      'company':'McDonalds',
-      'state':'CA',
-      'amount' : 20
-    },
-    headers: {
-      "content-type": "application/json"
+          this.setState( { menuTypes : types  },
+              ()=>{console.log()}
+          );
+
+        }).catch(function(error) {
+          console.log(error);
+        });
     }
-  }).then(function (response) {
-    console.log(response)
-  }).catch(function (error) {
-    console.log(error)
-  });
 
+    displayMenuTypes() {
+      const menu = this.state.menu
+      const menuTypes = this.state.menuTypes
+      return Object.keys(menu).map(menuType => {
+        var menuItems = menu[menuType]
+        var menuTypeToggle = menuTypes[menuType]
+        //https://stackoverflow.com/questions/46520847/using-map-to-access-nested-json-in-react-native
+        return (
+          <div key={menuType}>
+            <button onClick={() => this.toggleMenuItems( menuType )}>
+                {menuType}
+            </button>
+
+            <ul>
+                {menuTypeToggle ? this.displayMenuItemsOnToggle(menuItems) : ""}
+            </ul>
+          </div>
+
+        );
+      });
+    }
+
+    //change this to a form later for user input
+    displayMenuItemsOnToggle(menuItems) {
+      return Object.keys(menuItems).map(key => {
+        const item = menuItems[key]
+        const itemName = item['name']
+        const itemPrice = item['price']
+        return (
+          <li key={itemName}> {itemName} : ${itemPrice} </li>
+        );
+      });
+    }
+
+    displayRandomOrder() {
+      const randomOrder = this.state.randomOrders
+      //if empty = true, that means the django call returned an empty random order
+      if (randomOrder['empty'] !== true) {
+        return Object.keys(randomOrder).map(itemName => {
+          const itemPrice = randomOrder[itemName]['price']
+          const itemCount = randomOrder[itemName]['count']
+          return (
+            <li key={itemName}> {itemCount} {itemName} : ${itemPrice} </li>
+          );
+        });
+      }
+      else {
+        return (
+          <li> Could not return a random order for given amount </li>
+        );
+      }
+    }
+
+  getRandomOrder(amountGiven) {
+    if (isNaN(amountGiven) || amountGiven == null || amountGiven === "") {
+      alert("Enter a value before generating a random order")
+    }
+    else {
+      axios({
+        method: 'post',
+        url: 'http://localhost:8000/api/test',
+        data: {
+          'company':'McDonalds',
+          'state':'CA',
+          'amount' : amountGiven
+        },
+        headers: {
+          "content-type": "application/json"
+        }
+      }).then(res => {
+        // arrow functions (res => {}) automatically binds functions so calling this.setState won't throw an error
+        //seen here https://stackoverflow.com/questions/38238512/react-this-is-undefined
+        const randomOrder = res.data
+        this.setState( { randomOrders : randomOrder } )
+      }).catch(function (error) {
+        console.log(error)
+      });
+    }
   }
 
-  /*
-  The backticks are an ES6 template string, and it probably does what you think:
-  the ${...} part is replaced by the value of that expression, so the URL passed
-  to axios.get is actually http://www.reddit.com/r/reactjs.json.
-  */
+  //https://stackoverflow.com/questions/42761378/react-create-a-new-html-element-on-click
+  toggleMenuItems(menuTypeGiven) {
+    var types = this.state.menuTypes
+    var toggle = types[menuTypeGiven]
+    types[menuTypeGiven] = !toggle
+    this.setState( { menuTypes : types } )
+  }
+
+  handleChange(event) {
+    // pattern="[0-9]+([\.][0-9]+)?" regex for decimal numbers, if I ever want to implement another text input for change
+    if (event.target.value === "" || (!isNaN(event.target.value) && parseInt(event.target.value) <= 1000))
+      this.setState({amount: event.target.value});
+    else
+      alert("Input must be a number and under $1000")
+  }
+
   render() {
     const header = this.state.company + ", " + this.state.stateName
-    const menu = this.state.menu
-    const menuHTML = Object.keys(menu).map(menuType => {
-      //const menuItems = menu[menuType]
-      //https://stackoverflow.com/questions/46520847/using-map-to-access-nested-json-in-react-native
-      return (
-        <ul key={menuType}>
-          {menuType}
-
-        </ul>
-      );
-    });
 
     return (
       <div>
         <h1>{header}</h1>
         <div>
-          <ul> {menuHTML} </ul>
+          <ul> {this.displayMenuTypes()} </ul>
+           <label>
+             $ You Want To Spend
+             <input type="text"  value={this.state.amount} onChange={this.handleChange}/>
+           </label>
+          <button onClick={() => this.getRandomOrder(parseFloat(this.state.amount)) }>
+            Generate Random Order
+          </button>
+          {this.displayRandomOrder()}
         </div>
       </div>
+
     );
   }
 }
 
+
 ReactDOM.render(
-  <FetchDemo testApi="reactjs"/>,
+  <FetchDemo testApi="reactjs app"/>,
   document.getElementById('root')
 );
